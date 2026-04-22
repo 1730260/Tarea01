6c4188fdd696a20a117a9dcccb8ca6080ca8b84a
Leandro <1730260@senati.pe>
2026-04-22 15:52:15 -0500

    bromeando

---
+++ JS/primerJS.js
@@ -0,0 +1,13 @@
+// Console.log nos sirve para depurar y probar nuestras salidas
+console.log("Hola mundo desde JavaScript");
+//alert nos sirve para mostrarle un alert al usuario
+alert("Hola Mundo. Esto es una Alerta");
+
+function bromasTriste(){
+    document.getElementById("joker_feliz").src="../Images/joker triste.png";
+    alert("El bromas triste");
+}
+function bromasFeliz(){
+    document.getElementById("joker_triste").src="../Images/joker feliz.jpg";
+    alert("El bromas feliz");
+}
