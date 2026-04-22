1495d69d62152e473e9ff3fb18085e8486267349
Leandro <1730260@senati.pe>
2026-04-22 16:04:43 -0500

    joker

--- JS/primerJS.js
+++ JS/primerJS.js
@@ -4,10 +4,10 @@ console.log("Hola mundo desde JavaScript");
 alert("Hola Mundo. Esto es una Alerta");
 
 function bromasTriste(){
-    document.getElementById("joker_feliz").src="../Images/joker triste.png";
+    document.getElementById("joker").src="../Images/joker triste.png";
     alert("El bromas triste");
 }
 function bromasFeliz(){
-    document.getElementById("joker_triste").src="../Images/joker feliz.jpg";
+    document.getElementById("joker").src="../Images/joker feliz.jpg";
     alert("El bromas feliz");
 }
