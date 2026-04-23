ac05655e73813c07bbfbd0bf6ecb560a0cade589
Leandro <1730260@senati.pe>
2026-04-23 17:18:08 -0500

    probando

--- JS/tarea03.js
+++ JS/tarea03.js
@@ -5,41 +5,41 @@ function ejercicio9(){
     alert("Recorriste " + distKM + " km y su total a pagar es: " + total);
 }
 
-function mensajeAlert1() {
-    alert("")
+let cambios = 0;
+function abriendo_caja() {
+    if (cambios < 5) {
+        alert("me encontraste!");
+        cambios++;
+    } else {
+        alert("Que intentas, Romperme???");
+    }
 }
-function mensajeAlert2() {
-    alert("")
+function cerrando_caja() {
+    if (cambios > 0) {
+        alert("o no te equivocaste");cambios--;
+        cambios--;
+    } else {
+        alert("Ya estoy cerradooo!!!");
+    }
 }
-function mensajeAlert3() {
-    alert("")
-}
-function mensajeAlert4() {
-    alert("")
-}
-function mensajeAlert5() {
-    alert("")
-}
-function mensajeAlert6() {
-    alert("")
-}
-
 function cambio1() {
-    alert("me encontraste!")
+    alert("CAMBIOOOO!!!!");
+    document.getElementById("cambioTexto1").textContent = "¡Insecto insignificante!";
+    document.getElementById("capGinyu").src="../Images/capGinyu_cambio.gif";
 }
 function cambio2() {
-
+    alert("CAMBIOOOO!!!!");
+    document.getElementById("cambioTexto2").textContent = "¡Ni siquiera mereces que use todo mi poder!";
+    document.getElementById("capGinyu").src="../Images/capGinyu_cambio.gif";
 }
 function cambio3() {
-
+    alert("CAMBIOOOO!!!!");
+    document.getElementById("cambioTexto3").textContent = "¡Patético!";
+    document.getElementById("capGinyu").src="../Images/capGinyu_cambio.gif";
 }
-function cambio4() {
 
-}
-
-function cambioText1() {
-
-}
-function cambioText2() {
-
+function rana() {
+    alert("¿Una rana?");
+    document.querySelector(".rana").textContent = "Croac!"
+    document.getElementById("capGinyu").src = "../Images/ginyu_rana.jpg";
 }
