b9f40d74dde3da5ae4d3b6f19d53e155cf870c21
Leandro <1730260@senati.pe>
2026-04-23 15:29:14 -0500

    p

---
+++ JS/tarea03.js
@@ -0,0 +1,45 @@
+function ejercicio9(){
+    let distKM = 3;
+    let tarifaB = 10, km = 3;
+    let total = distKM * km + tarifaB;
+    alert("Recorriste " + distKM + " km y su total a pagar es: " + total);
+}
+
+function mensajeAlert1() {
+    alert("")
+}
+function mensajeAlert2() {
+    alert("")
+}
+function mensajeAlert3() {
+    alert("")
+}
+function mensajeAlert4() {
+    alert("")
+}
+function mensajeAlert5() {
+    alert("")
+}
+function mensajeAlert6() {
+    alert("")
+}
+
+function cambio1() {
+    
+}
+function cambio2() {
+
+}
+function cambio3() {
+
+}
+function cambio4() {
+
+}
+
+function cambioText1() {
+
+}
+function cambioText2() {
+
+}
